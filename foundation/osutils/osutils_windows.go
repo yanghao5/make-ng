d3b4b3c31ca260b53a42b989c7e2b0c7afd3c766
yanghao5 <yanghao.buffer@outlook.com>
2025-04-30 23:11:34 +0800

    [comment](foundation)(osutils)(windows): add comment for WinOS()

--- foundation/osutils/osutils_windows.go
+++ foundation/osutils/osutils_windows.go
@@ -2,16 +2,18 @@ package osutils
 
 import (
 	"fmt"
+	"log"
 	"runtime"
 
 	"golang.org/x/sys/windows"
 )
 
 // `WinOS` get windows version
-func WinOS() (string, error) {
+func WinOS() string {
 	if runtime.GOOS != "windows" {
-		return "", fmt.Errorf("\033[31merror\033[0m: WinOS() unsupported OS: %s, only Windows is supported", runtime.GOOS)
+		log.Printf("\033[31merror\033[0m: WinOS() unsupported OS: %s, only Windows is supported", runtime.GOOS)
+		return ""
 	}
 	maj, min, patch := windows.RtlGetNtVersionNumbers()
-	return fmt.Sprintf("%d.%d.%d", maj, min, patch), nil
+	return fmt.Sprintf("%d.%d.%d", maj, min, patch)
 }
