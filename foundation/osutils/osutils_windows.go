d93cc166ef0181a57c14024fc25c6fa04df65a60
yanghao5 <yanghao.buffer@outlook.com>
2025-04-30 13:46:41 +0800

    [refine]: refine project struct

--- foundation/osutils/osutils_windows.go
+++ foundation/osutils/osutils_windows.go
@@ -7,6 +7,7 @@ import (
 	"golang.org/x/sys/windows"
 )
 
+// `WinOS` get windows version
 func WinOS() (string, error) {
 	if runtime.GOOS != "windows" {
 		return "", fmt.Errorf("\033[31merror\033[0m: WinOS() unsupported OS: %s, only Windows is supported", runtime.GOOS)
