375c903be8cbb47a227728473aa60f5e42538205
yanghao5 <yanghao.buffer@outlook.com>
2025-04-30 23:12:48 +0800

    [feat](foundation)(osutils)(unix): add unix uname

---
+++ foundation/osutils/osutils_uname_unix.go
@@ -0,0 +1,12 @@
+package osutils
+
+import "golang.org/x/sys/unix"
+
+func uname() (*unix.Utsname, error) {
+	uts := &unix.Utsname{}
+
+	if err := unix.Uname(uts); err != nil {
+		return nil, err
+	}
+	return uts, nil
+}
